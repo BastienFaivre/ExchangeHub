7b553a419fae078136401cd1540d98665f7b1985
Philip Patrick Jan Hamelink <philip.hamelink@epfl.ch>
2022-04-06 09:51:07 +0200

    Deployment done

--- src/coursesAPI.js
+++ src/coursesAPI.js
@@ -1,20 +1,30 @@
-const BASE_URL = "https://www.kth.se/api/kopps/v2/";
-const ENGLISH = "&l=en";
+const BASE_URL = "https://www.kth.se/api/kopps/v2/"
+const ENGLISH = "&l=en"
 
 function treatHTTPResponseACB(res) {
-    console.log(res);
-    if (!res.ok) throw new Error("API problem: " + Response.status + "\n");
-    return res.json();
+	console.log(res)
+	if (!res.ok) throw new Error("API problem: " + Response.status + "\n")
+	return res.json()
 }
 
 function transformResultACB(res) {
-    return res.searchHits;
+	return res.searchHits
 }
 
 export function searchCourses(apiParam) {
-    return fetch("https://cors-anywhere.herokuapp.com/" + BASE_URL + "courses/search?" + new URLSearchParams(apiParam) + ENGLISH).then(treatHTTPResponseACB).then(transformResultACB);
+	// return fetch("https://cors-anywhere.herokuapp.com/" + BASE_URL + "courses/search?" + new URLSearchParams(apiParam) + ENGLISH).then(treatHTTPResponseACB).then(transformResultACB);
+	return fetch(
+		BASE_URL + "courses/search?" + new URLSearchParams(apiParam) + ENGLISH,
+		{
+			headers: {
+				mode: "no-cors",
+			},
+		}
+	)
+		.then(treatHTTPResponseACB)
+		.then(transformResultACB)
 }
 
 export function getCourseDetails(courseCode) {
-    return fetch(BASE_URL + "course/" + courseCode).then(treatHTTPResponseACB);
+	return fetch(BASE_URL + "course/" + courseCode).then(treatHTTPResponseACB)
 }
