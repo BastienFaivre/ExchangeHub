5a9b98b9a2a86489867a0eda6895b6253f9b56c7
Bastien Faivre <faivre.bast@gmail.com>
2022-04-07 14:50:09 +0200

    save work

--- src/mainContent/courses/searchFormView.js
+++ src/mainContent/courses/searchFormView.js
@@ -3,7 +3,7 @@ import InputLabel from "@mui/material/InputLabel"
 import MenuItem from "@mui/material/MenuItem"
 import FormControl from "@mui/material/FormControl"
 import Select from "@mui/material/Select"
-import Stack from "@mui/material/Stack"
+import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
 import InputBase from "@mui/material/InputBase"
 import IconButton from "@mui/material/IconButton"
@@ -12,6 +12,8 @@ import { useState } from "react"
 
 export default function SearchFormView({ search }) {
 	const [searchInput, setSearchInput] = useState("")
+	const [searchSchool, setSearchSchool] = useState("")
+	const [searchDepartment, setSearchDepartment] = useState("")
 
 	function handleInputChangeACB(event) {
 		setSearchInput(event.target.value)
@@ -23,29 +25,54 @@ export default function SearchFormView({ search }) {
 	}
 
 	return (
-		<Paper
-			component="form"
-			sx={{
-				p: "2px 4px",
-				display: "flex",
-				alignItems: "center",
-				width: 400,
-			}}>
-			<InputBase
-				sx={{ ml: 1, flex: 1 }}
-				placeholder="Search For Course"
-				inputProps={{ "aria-label": "search course" }}
-				value={searchInput}
-				onChange={handleInputChangeACB}
-			/>
-			<IconButton
-				type="submit"
-				sx={{ p: "10px" }}
-				aria-label="search"
-				onClick={doSearchACB}>
-				<SearchIcon />
-			</IconButton>
-		</Paper>
+		<Grid container spacing={2}>
+			<Grid item xs={12} lg={4}>
+				<Paper
+					component="form"
+					sx={{
+						p: "2px 4px",
+						display: "flex",
+						alignItems: "center",
+					}}>
+					<InputBase
+						sx={{ ml: 1, flex: 1 }}
+						placeholder="Search For Course"
+						inputProps={{ "aria-label": "search course" }}
+						value={searchInput}
+						onChange={handleInputChangeACB}
+					/>
+					<IconButton
+						type="submit"
+						sx={{ p: "10px" }}
+						aria-label="search"
+						onClick={doSearchACB}>
+						<SearchIcon />
+					</IconButton>
+				</Paper>
+			</Grid>
+			<Grid item xs={6} lg={4}>
+				<FormControl sx={{ width: "100%" }}>
+					<InputLabel>School</InputLabel>
+					<Select
+						value={search}
+						label="School"
+					>
+						<MenuItem value="">All</MenuItem>
+					</Select>
+				</FormControl>
+			</Grid>
+			<Grid item xs={6} lg={4}>
+				<FormControl sx={{ width: "100%" }}>
+					<InputLabel>Department</InputLabel>
+					<Select
+						value={""}
+						label="Department"
+					>
+						<MenuItem value="">All</MenuItem>
+					</Select>
+				</FormControl>
+			</Grid>
+		</Grid>
 		// <Stack direction="row" spacing={2} padding="20px">
 		//     <TextField id="outlined-basic" label="Course" variant="outlined" onChange={inputChangedACB} />
 		//     <Button
---
+++ src/utils/departments.js
@@ -0,0 +1,656 @@
+export default departments = [
+    {
+      "departmentPrefix": "K",
+      "name": "Chemical Science and Engineering",
+      "departments": [
+        {
+          "code": "KA",
+          "name": "Chemical Science and Engineering"
+        },
+        {
+          "code": "KD",
+          "name": "Chemistry"
+        },
+        {
+          "code": "KE",
+          "name": "Chemical Engineering and Technology"
+        },
+        {
+          "code": "KET",
+          "name": "Chemical Technology"
+        },
+        {
+          "code": "KF",
+          "name": "Fibre and Polymer Technology"
+        },
+        {
+          "code": "KH",
+          "name": "Engineering Pedagogics"
+        },
+        {
+          "code": "KHB",
+          "name": "Degree progr in Chemical Engineering"
+        },
+        {
+          "code": "KHD",
+          "name": "Technical Preperatory Year"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "M",
+      "name": "Industrial Engineering and Management",
+      "departments": [
+        {
+          "code": "MAC",
+          "name": "School of Industrial Engineering and Management"
+        },
+        {
+          "code": "MD",
+          "name": "Energy Technology"
+        },
+        {
+          "code": "ME",
+          "name": "Industrial Economics and Management"
+        },
+        {
+          "code": "MF",
+          "name": "Machine Design"
+        },
+        {
+          "code": "MG",
+          "name": "Production Engineering"
+        },
+        {
+          "code": "MH",
+          "name": "Materials Science and Engineering"
+        },
+        {
+          "code": "MJ",
+          "name": "Energy Technology"
+        },
+        {
+          "code": "MJEA",
+          "name": "Energy Technology"
+        },
+        {
+          "code": "MJEC",
+          "name": "Heat and Power Technology"
+        },
+        {
+          "code": "ML",
+          "name": "Sustainable Production Development"
+        },
+        {
+          "code": "MO",
+          "name": "Learning in Engineering Sciences"
+        },
+        {
+          "code": "MV",
+          "name": "Materials Science and Engineering"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "A",
+      "name": "Architecture and the Built Environment",
+      "departments": [
+        {
+          "code": "AAB",
+          "name": "School of Architecture and the Built Environment"
+        },
+        {
+          "code": "ADB",
+          "name": "Architecture"
+        },
+        {
+          "code": "ADC",
+          "name": "Architecture"
+        },
+        {
+          "code": "ADD",
+          "name": "Architecture"
+        },
+        {
+          "code": "ADF",
+          "name": "Lighting Design"
+        },
+        {
+          "code": "AFA",
+          "name": "Civil and Architectural Engineering"
+        },
+        {
+          "code": "AFB",
+          "name": "Building Materials"
+        },
+        {
+          "code": "AFC",
+          "name": "Building Technology"
+        },
+        {
+          "code": "AFD",
+          "name": "Building Services and Energy Systems"
+        },
+        {
+          "code": "AFE",
+          "name": "Structural Design & Bridges"
+        },
+        {
+          "code": "AFF",
+          "name": "Concrete Structures"
+        },
+        {
+          "code": "AFH",
+          "name": "Soil- and Rock Mechanics"
+        },
+        {
+          "code": "AFK",
+          "name": "Constructional Engineering and Design"
+        },
+        {
+          "code": "AFL",
+          "name": "Fluid and Climate Technology"
+        },
+        {
+          "code": "AFP",
+          "name": "Transport Planning"
+        },
+        {
+          "code": "AFT",
+          "name": "Sustainable Buildings"
+        },
+        {
+          "code": "AG",
+          "name": "Urban Planning and Environment"
+        },
+        {
+          "code": "AGB",
+          "name": "Urban and Regional Studies"
+        },
+        {
+          "code": "AGF",
+          "name": "Geoinformatics"
+        },
+        {
+          "code": "AGI",
+          "name": "Transport and Systems Analysis"
+        },
+        {
+          "code": "AIB",
+          "name": "Real Estate Economics and Finance"
+        },
+        {
+          "code": "AIC",
+          "name": "Real Estate Planning and Land Law"
+        },
+        {
+          "code": "AID",
+          "name": "Construction and Facilities Management"
+        },
+        {
+          "code": "AIE",
+          "name": "Real Estate Business and Financial Systems"
+        },
+        {
+          "code": "AIG",
+          "name": "Geodesy and Satellite Positioning"
+        },
+        {
+          "code": "AKB",
+          "name": "Philosophy"
+        },
+        {
+          "code": "AKC",
+          "name": "History of Science, Technology and Environment"
+        },
+        {
+          "code": "AL",
+          "name": "Sustainable development, environmental science and engineering"
+        },
+        {
+          "code": "ALF",
+          "name": "Sustainability and Environmental Engineering"
+        },
+        {
+          "code": "AMB",
+          "name": "Contract Education: Urban Development and Design"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "H",
+      "name": "Technology and Health",
+      "departments": [
+        {
+          "code": "HA",
+          "name": "Technology and Health"
+        },
+        {
+          "code": "HLA",
+          "name": "Environmental Physiology"
+        },
+        {
+          "code": "HLF",
+          "name": "Basic Science"
+        },
+        {
+          "code": "HLK",
+          "name": "Neuronic"
+        },
+        {
+          "code": "HLL",
+          "name": "Medical Technology"
+        },
+        {
+          "code": "HLM",
+          "name": "Structural Biotechnology"
+        },
+        {
+          "code": "HPC",
+          "name": "System Safety and Management"
+        },
+        {
+          "code": "HPCA",
+          "name": "Technology in Health Care"
+        },
+        {
+          "code": "HPE",
+          "name": "Unit for Health Innovation"
+        },
+        {
+          "code": "HPG",
+          "name": "DASH"
+        },
+        {
+          "code": "HPM",
+          "name": "Health Care Logistics"
+        },
+        {
+          "code": "HPN",
+          "name": "Ergonomics"
+        },
+        {
+          "code": "HSE",
+          "name": "Computer and Electrical Engineering"
+        },
+        {
+          "code": "HSL",
+          "name": "Medical Imaging"
+        },
+        {
+          "code": "HSM",
+          "name": "Medical Imaging and Visualization"
+        },
+        {
+          "code": "HSN",
+          "name": "Neuronic Engineering"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "L",
+      "name": "Education and Communication in Engineering Science",
+      "departments": [
+        {
+          "code": "L",
+          "name": "School of Education and Communication"
+        },
+        {
+          "code": "LAC",
+          "name": "Student Administration Office"
+        },
+        {
+          "code": "LGC",
+          "name": "Library"
+        },
+        {
+          "code": "LJ",
+          "name": "Learning Lab"
+        },
+        {
+          "code": "LP",
+          "name": "Learning"
+        },
+        {
+          "code": "LPA",
+          "name": "Teacher Education"
+        },
+        {
+          "code": "LPB",
+          "name": "Teaching and Learning"
+        },
+        {
+          "code": "LS",
+          "name": "Language and Communication"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "C",
+      "name": "Engineering Sciences in Chemistry, Biotechnology and Health",
+      "departments": [
+        {
+          "code": "CB",
+          "name": "School-wide"
+        },
+        {
+          "code": "CD",
+          "name": "Biomedical Engineering and Health Systems"
+        },
+        {
+          "code": "CE",
+          "name": "Chemistry"
+        },
+        {
+          "code": "CF",
+          "name": "Chemical Engineering"
+        },
+        {
+          "code": "CG",
+          "name": "Fibre and Polymer Technology"
+        },
+        {
+          "code": "CH",
+          "name": "Gene Technology"
+        },
+        {
+          "code": "CJ",
+          "name": "Protein Science"
+        },
+        {
+          "code": "CK",
+          "name": "Industrial Biotechnology"
+        },
+        {
+          "code": "CL",
+          "name": "Theoretical Chemistry and Biology"
+        },
+        {
+          "code": "CM",
+          "name": "Engineering Pedagogics"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "B",
+      "name": "Biotechnology",
+      "departments": [
+        {
+          "code": "BA",
+          "name": "Biotechnology"
+        },
+        {
+          "code": "BB",
+          "name": "Biotechnology"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "J",
+      "name": "Electrical Engineering and Computer Science",
+      "departments": [
+        {
+          "code": "JF",
+          "name": "Communication"
+        },
+        {
+          "code": "JG",
+          "name": "Electronics and Embedded Systems"
+        },
+        {
+          "code": "JH",
+          "name": "Computer Science"
+        },
+        {
+          "code": "JHF",
+          "name": "Communication Systems"
+        },
+        {
+          "code": "JHK",
+          "name": "Software and Computer Systems"
+        },
+        {
+          "code": "JHP",
+          "name": "Network and Systems Engineering"
+        },
+        {
+          "code": "JHS",
+          "name": "Computational Science and  Technology"
+        },
+        {
+          "code": "JHT",
+          "name": "Theoretical Computer Science"
+        },
+        {
+          "code": "JJ",
+          "name": "Electrical Engineering"
+        },
+        {
+          "code": "JJD",
+          "name": "Fusion Plasma Physics"
+        },
+        {
+          "code": "JJE",
+          "name": "Space and Plasma Physics"
+        },
+        {
+          "code": "JJG",
+          "name": "Electronics and Embedded Systems"
+        },
+        {
+          "code": "JJI",
+          "name": "Electromagnetic Engineering"
+        },
+        {
+          "code": "JJN",
+          "name": "Electric Power and Energy Systems"
+        },
+        {
+          "code": "JM",
+          "name": "Human Centered Technology"
+        },
+        {
+          "code": "JMA",
+          "name": "Media Technology and Interaction Design"
+        },
+        {
+          "code": "JR",
+          "name": "Intelligent Systems"
+        },
+        {
+          "code": "JRL",
+          "name": "Decision and Control Systems"
+        },
+        {
+          "code": "JRO",
+          "name": "Information Science and Engineering"
+        },
+        {
+          "code": "JRQ",
+          "name": "Micro and Nano Systems"
+        },
+        {
+          "code": "JRR",
+          "name": "Robotics, Perception and Learning"
+        },
+        {
+          "code": "JRT",
+          "name": "Speech, Music and Hearing"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "D",
+      "name": "Computer Science and Communication",
+      "departments": [
+        {
+          "code": "DA",
+          "name": "Computer Science and Communication"
+        },
+        {
+          "code": "DAG",
+          "name": "Computer Science and Communication"
+        },
+        {
+          "code": "DD",
+          "name": "Computer Science"
+        },
+        {
+          "code": "DH",
+          "name": "Human-Computer Interaction"
+        },
+        {
+          "code": "DK",
+          "name": "computational Science and Technology"
+        },
+        {
+          "code": "DL",
+          "name": "Theoretical Computer Science"
+        },
+        {
+          "code": "DM",
+          "name": "Media Technology and Interaction Design"
+        },
+        {
+          "code": "DME",
+          "name": "Media Technology and Interaction Design"
+        },
+        {
+          "code": "DO",
+          "name": "Robotics, Perception and Learning"
+        },
+        {
+          "code": "DT",
+          "name": "Speech, Music and Hearing"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "E",
+      "name": "Electrical Engineering",
+      "departments": [
+        {
+          "code": "ED",
+          "name": "Fusion Plasma Physics"
+        },
+        {
+          "code": "EF",
+          "name": "Space and Plasma Physics"
+        },
+        {
+          "code": "EI",
+          "name": "Electromagnetic Engineering"
+        },
+        {
+          "code": "EK",
+          "name": "Micro and Nanosystems"
+        },
+        {
+          "code": "EL",
+          "name": "Automatic Control"
+        },
+        {
+          "code": "EN",
+          "name": "Electric Power and Energy Systems"
+        },
+        {
+          "code": "EO",
+          "name": "Information Science and Engineering"
+        },
+        {
+          "code": "EP",
+          "name": "Network and Systems Engineering"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "S",
+      "name": "Engineering Sciences",
+      "departments": [
+        {
+          "code": "SA",
+          "name": "Engineering Sciences"
+        },
+        {
+          "code": "SD",
+          "name": "Aeronautical and Vehicle Engineering"
+        },
+        {
+          "code": "SE",
+          "name": "Solid Mechanics"
+        },
+        {
+          "code": "SF",
+          "name": "Mathematics"
+        },
+        {
+          "code": "SG",
+          "name": "Mechanics"
+        },
+        {
+          "code": "SH",
+          "name": "Physics"
+        },
+        {
+          "code": "SHB",
+          "name": "Undergraduate Physics"
+        },
+        {
+          "code": "SI",
+          "name": "Theoretical Physics"
+        },
+        {
+          "code": "SIB",
+          "name": "Theoretical Undergaduate Physics"
+        },
+        {
+          "code": "SK",
+          "name": "Applied Physics"
+        },
+        {
+          "code": "SKB",
+          "name": "Applied undergraduate Physics"
+        },
+        {
+          "code": "SM",
+          "name": "Engineering Mechanics"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "U",
+      "name": "Cooperation with other universities",
+      "departments": [
+        {
+          "code": "UL",
+          "name": "Stockholm University"
+        }
+      ]
+    },
+    {
+      "departmentPrefix": "I",
+      "name": "Information and Communication Technology",
+      "departments": [
+        {
+          "code": "IA",
+          "name": "Information and Communication Technology"
+        },
+        {
+          "code": "IE",
+          "name": "Electronic Systems"
+        },
+        {
+          "code": "IF",
+          "name": "Communications Systems"
+        },
+        {
+          "code": "IG",
+          "name": "Electronics"
+        },
+        {
+          "code": "IK",
+          "name": "Software and Computer system"
+        }
+      ]
+    }
+  ]
