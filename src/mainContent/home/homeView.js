d624afaf4cc010529f32b9c17d80c1dd88a0069c
Bastien Faivre <faivre.bast@gmail.com>
2022-05-09 20:48:42 +0200

    feat: adjust home page

--- src/mainContent/home/homeView.js
+++ src/mainContent/home/homeView.js
@@ -2,25 +2,27 @@ import React from "react"
 import { Grid, Typography, Button, Box } from "@mui/material"
 import exchangestudent from "../../images/exchangestudent.jpeg"
 import styles from "../../MUI/styles/styles"
+import { Link } from "react-router-dom"
 
 export default function HomeView() {
     return (
         <Grid container sx={styles.gridContainer}>
             <Grid item xs={12} md={7} p={4}>
                 <Typography variant="h3" fontWeight={700} sx={styles.title}>
-                    Everything you need to go on exchange
+                    Everything you need to study at KTH
                 </Typography>
                 <Typography variant="h6" sx={styles.subtitle}>
                     Find information about the courses, accomodation, and
-                    lifestyle about you hosting school written by other
-                    students.
+                    lifestyle about KTH and Stockholm written by other students.
                 </Typography>
                 <Button
                     variant="contained"
                     color="primary"
                     sx={{ width: "200px", fontSize: "16px" }}
+                    component={Link}
+                    to={"/students"}
                 >
-                    FIND YOUR SCHOOL
+                    Browse students
                 </Button>
             </Grid>
             <Grid item xs={12} md={5}>
