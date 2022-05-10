4400306c03a8c0fbf7c816fbeecea06e995b775c
Bastien Faivre <faivre.bast@gmail.com>
2022-05-10 19:26:23 +0200

    fix: remove unused imports

--- src/MUI/theme.js
+++ src/MUI/theme.js
@@ -1,5 +1,4 @@
 import { createTheme } from "@mui/material/styles"
-import { grey } from "@mui/material/colors"
 
 const theme = createTheme({
     palette: {
@@ -23,20 +22,5 @@ const theme = createTheme({
         },
     },
 })
-
-// Example custom theme
-// const theme = createTheme({
-// 	palette: {
-// 		type: "light",
-// 		primary: {
-// 			main: "#1a237e",
-// 		},
-// 		secondary: {
-// 			main: "#d81b60",
-// 		},
-// 	},
-// })
-
-// Feel free to change theme with theme generator on https://bareynol.github.io/mui-theme-creator/
 
 export default theme
